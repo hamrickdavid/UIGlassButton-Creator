1edd3e7ee7b20024974ba03171a88e342be38c22
David Hamrick <hamrickdavid@gmail.com>
2011-07-13 19:48:12 -0500

    Initial Commit

---
+++ GlassButtonCreator/GlassButtonCreatorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GlassButtonCreatorAppDelegate.h
+//  GlassButtonCreator
+//
+//  Created by David Hamrick on 7/13/11.
+//  Copyright 2011 David Hamrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GlassButtonCreatorAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
